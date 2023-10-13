4a5823d82b04c1eb2d69628db592e8aea6580f55
ghost1nwires <kaorodevil999@gmail.com>
2023-10-13 12:27:24 +0800

    learn debug

---
+++ Chapter3 Debugging C++ Programs/3.1 Syntax and semantic errors/division_byzero/division_byzero.cpp
@@ -0,0 +1,15 @@
+// division_byzero.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int d {10};
+    int z{ 0 };
+
+    std::cout << a << "/" << b << "=" << a / b << '/n';
+    return 0;
+}
+
+// division by 0 is undefined in mathematics
